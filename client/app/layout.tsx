4b32286cb17e563162e3b7a7e3aeca4d97052147
scottxxxxx <github@weirtech.com>
2026-03-19 00:40:53 -0500

    Add Google Analytics (G-94DE1FGP0D) to RedGuard

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -29,6 +29,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-94DE1FGP0D" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-94DE1FGP0D');
+            `,
+          }}
+        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
